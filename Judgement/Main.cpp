27dc42de75e66880fc22524fd72d6e6b1056081b
Yuki-Mori <yuki7291@pluslab.org>
2017-07-13 14:38:49 +0900

    Mainクラスの作成
    JM::Mainクラスの作成

---
+++ Judgement/Main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   Main.cpp
+ * Author: Yuki Mori
+ *
+ * Created on 2017/07/13
+ */
+#include "Main.h"
+using namespace JM;
+
+Main::Main(void) {
+}
+
+Main::~Main(void) {
+    
+}
+
+void Main::run(void) {
+    
+}
---
+++ Judgement/Main.h
@@ -0,0 +1,18 @@
+/*
+ * File:   Main.h
+ * Author: Yuki Mori
+ *
+ * Created on 2017/07/13
+ */
+#ifndef __MAIN
+#define __MAIN
+namespace JM {
+    class Main {
+    public:
+        Main(void);
+        ~Main(void);
+        void run(void);
+    private:
+    };
+}
+#endif /*__MAIN*/
