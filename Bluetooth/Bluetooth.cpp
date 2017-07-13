fe6a49f85593f0908821eb6ae20c2c830d9587f2
Yuki-Mori <yuki7291@pluslab.org>
2017-07-13 16:07:47 +0900

    メソッドの追加
    printメソッドとgetStringメソッドの追加

--- Bluetooth/Bluetooth.cpp
+++ Bluetooth/Bluetooth.cpp
@@ -7,7 +7,38 @@
 #include "Bluetooth.h"
 
 using namespace BT;
+using namespace AITLibrary;
 
-Bluetooth::Bluetooth(void){
+Bluetooth::Bluetooth(void): cn() {
 	
+}
+
+Bluetooth::Bluetooth(BTConnection cn): cn(cn) {
+	
+}
+
+Bluetooth::~Bluetooth(void) {
+	
+}
+
+bool Bluetooth::print(String str) {
+	return this->cn.write(str.getCharPointer());
+}
+
+String Bluetooth::getString(void) {
+	if(!cn.isConnected()) {
+		return "";
+	}
+	char c = cn.read();
+	String str = "";
+	while(c != '\r') {
+		if(c < 0) {
+			return "";
+		}
+		char tmp[2];
+		tmp[0] = c;
+		tmp[1] = '\0';
+		str += tmp;
+	}
+	return str;
 }
--- Bluetooth/Bluetooth.h
+++ Bluetooth/Bluetooth.h
@@ -7,12 +7,16 @@
 #ifndef __BLUETOOTH
 #define __BLUETOOTH
 #include "BTConnection.h"
+#include "../Library/MyString.h"
+using namespace AITLibrary;
 namespace BT {
 	class Bluetooth{
 	public:
 		Bluetooth(void);
 		Bluetooth(BTConnection cn);
 		~Bluetooth(void);
+		bool print(String str);
+		String getString(void);
 	private:
 		BTConnection cn;
 	};
